285cb56fd1418412adf1f8265758becd47c3412d
pdh02189 <pdh02189@naver.com>
2024-04-14 19:52:28 +0900

    100%~!!!
    100%~!!!

--- script/main.js
+++ script/main.js
@@ -114,3 +114,92 @@ $(document).mouseup(function (e){
 		$('.m_search_popup').css('display', 'none');
     }
 });
+
+// 등록폼 이미지 업로드
+document.addEventListener('DOMContentLoaded', function(){
+  //이미지 객체 타입으로 이미지 확장자 밸리데이션
+  var validateType = function(img){
+      return (['image/jpeg','image/jpg','image/png'].indexOf(img.type) > -1);
+  }
+
+  var validateName = function(fname){
+      let extensions = ['jpeg','jpg','png'];
+      let fparts = fname.split('.');
+      let fext = '';
+  
+      if(fparts.length > 1){
+          fext = fparts[fparts.length-1];
+      }
+  
+      let validated = false;
+      
+      if(fext != ''){
+          extensions.forEach(function(ext){
+              if(ext == fext){
+                  validated = true;
+              }
+          });
+      }
+  
+      return validated;
+  }
+
+  // 파일 선택 필드에 이벤트 리스너 등록
+  // document.querySelector('.imageSelector').addEventListener('change', function(e){
+  //     let elem = e.target;
+  //     if(validateType(elem.files[0])){
+  //         let preview = document.querySelector('.thumb img');
+  //         preview.src = URL.createObjectURL(elem.files[0]); //파일 객체에서 이미지 데이터 가져옴.
+  //         document.querySelector('.thumb').style.display = 'block'; // 이미지 삭제 링크 표시
+  //         preview.onload = function() {
+  //             URL.revokeObjectURL(preview.src); //URL 객체 해제
+  //         }
+  //     }else{
+  //     console.log('이미지 파일이 아닙니다.');
+  //     }
+  // });
+  // document.querySelector('.dellink').addEventListener('click', function(e){
+  //     let dellink = e.target;
+  //     let preview = dellink.previousElementSibling;
+  //     preview.src = ''; // 썸네일 이미지 src 데이터 해제
+  //     document.querySelector('.thumb').style.display = 'none';
+  // });
+ 
+});
+
+$(function() {
+  $(".imageSelector").off("change").on("change", function(e) {
+    var imageSelector = $(this);
+    var file = e.target.files[0];
+    if(isImageFile(file)) {
+      var reader = new FileReader(); 
+      reader.onload = function(e) {
+        imageSelector.parent().siblings(".thumb").children("img").attr("src", e.target.result);
+        imageSelector.parent().siblings(".thumb").removeClass("d_none");
+      }
+      reader.readAsDataURL(file);
+    } else {
+      alert("이미지 파일만 첨부 가능합니다.");
+      imageSelector.val("");
+      imageSelector.parent().siblings(".thumb").children("img").attr("src", "");
+    }
+  });
+
+  $(".dellink").on("click", function(e) {
+      var delbtn = $(this);
+      let dellink = e.target;
+      let preview = dellink.previousElementSibling;
+      preview.src = ''; // 썸네일 이미지 src 데이터 해제
+      delbtn.parent(".thumb").addClass("d_none");
+      delbtn.parent(".thumb").siblings("label").children(".imageSelector").val("");
+  });
+});
+
+// 업로드 파일 이미지 파일인지 확인
+function isImageFile(file) {
+  // 파일명에서 확장자를 가져옴
+  var ext = file.name.split(".").pop().toLowerCase(); 
+  return ($.inArray(ext, ["jpg", "jpeg", "gif", "png"]) === -1) ? false : true;
+}
+
+
